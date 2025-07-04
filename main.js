778c1610d76366bd30409351486d8995a7249167
TheMazeMaster <the_maze_master@yahoo.com>
2025-07-04 22:20:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const NORMAL_SCALE = 1.4;
 const NORMAL_OFFSET_X = -142;
 const NORMAL_OFFSET_Y = -145;
 const ZOOM_SCALE = 1.8;
-const ZOOM_OFFSET_X = -250;
+const ZOOM_OFFSET_X = -270;
 const ZOOM_OFFSET_Y = -215;
 
 function updateViewport() {
